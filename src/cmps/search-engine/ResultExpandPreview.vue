137438f813799a24adc45008c27c8aae04e17646
drznr <dresnerdaniel@gmail.com>
2020-12-30 17:58:40 +0200

    expandable SE result started

---
+++ src/cmps/search-engine/ResultExpandPreview.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    {{ interaction.side2Label._id }}
+  </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            vInteractions: []
+        }
+    },
+    props: {
+        interaction: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        async getRelatedMaterials() {
+            const { interaction } = this;
+            const labelId = interaction.side2Label._id
+            const criteria = { limit: 0, labelId };
+            const relatedMaterials = await this.$store.dispatch({ type: 'getMaterials', criteria });
+            this.vInteractions = relatedMaterials.map(({ _id, name, type, atcPaths }) => {
+                return {
+                    _id: interaction._id,
+                    side1Material: interaction.side1Material,
+                    side2Material: {
+                        _id,
+                        name,
+                        type
+                    },
+                    side2Label: null,
+                    recommendation: interaction.recommendation,
+                    isVirual: true,
+                    atcPaths
+                }
+            });
+            console.log(this.vInteractions);
+        }
+    },
+    created() {
+        this.getRelatedMaterials();
+    }
+}
+</script>
--- src/cmps/search-engine/ResultList.vue
+++ src/cmps/search-engine/ResultList.vue
@@ -1,26 +1,52 @@
 <template>
   <section class="results-list">
     <v-chip-group column v-if="!isLoading">
-      <v-chip
-        class="mb-4"
+      <span
         v-for="interaction in interactions"
         :key="interaction._id"
-        outlined
-        :color="getInteractionColor(interaction)"
       >
-        <router-link 
-          class="results-list-link"
-          :to="(interaction.isVirual) ? `/interaction/${interaction._id}/${interaction.side2Material._id}` : `/interaction/${interaction._id}`" 
+        <v-chip
+          class="mb-4"
+          v-if="!interaction.side2Label"
+          outlined
+          :color="getInteractionColor(interaction)"
         >
-          {{ getInteractionName(interaction) }}
-        </router-link>
-      </v-chip>
+          <router-link 
+            class="results-list-link"
+            :to="(interaction.isVirual) ? `/interaction/${interaction._id}/${interaction.side2Material._id}` : `/interaction/${interaction._id}`" 
+          >
+            {{ getInteractionName(interaction) }}
+          </router-link>
+        </v-chip>
+        <v-expansion-panels v-else flat class="results-list-expand-panel">
+          <v-expansion-panel class="results-list-expand-panel-panel">
+            <v-expansion-panel-header 
+              class="results-list-expand-panel-panel-header pa-0"
+              disable-icon-rotate
+              hide-actions
+            >
+              <v-chip
+                class="results-list-expand-panel-panel-chip mb-4"
+                outlined
+                :color="getInteractionColor(interaction)"
+              >
+                {{ getInteractionName(interaction) }}
+                <v-icon small class="ml-2">mdi-family-tree</v-icon>
+              </v-chip>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <result-expand-preview :interaction="interaction" />
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </span>
     </v-chip-group>
     <loader v-else />
   </section>
 </template>
 
 <script>
+import resultExpandPreview from '@/cmps/search-engine/ResultExpandPreview';
 import loader from '@/cmps/general/LoadingCmp';
 
 export default {
@@ -60,6 +86,7 @@ export default {
     }
   },
   components: {
+    resultExpandPreview,
     loader
   }
 }
