6e3d5ef89b0abd06d7613941a6b000bf45481505
drznr <dresnerdaniel@gmail.com>
2021-02-10 15:38:00 +0200

    vinteraction refactor started

--- src/views/interaction/VinteractionDetails.vue
+++ src/views/interaction/VinteractionDetails.vue
@@ -6,108 +6,123 @@
                     <v-icon small left>mdi-arrow-left</v-icon>Back
                 </v-btn>
             </div>
-            <v-card
-                v-if="interaction && material"
-                class="v-interaction-details-content px-2"
-            >
-                <v-card-title class="v-interaction-details-title px-0 justify-center my-4">
-                    <h2>
-                        {{ interaction.side1Material.name }}
-                        <span>&amp;</span>
-                        {{ material.name }}
-                    </h2>
-                </v-card-title>
-
-                <div class="text-capitalize">Recommendation:</div>
-                <div v-recommendation-color>{{ interaction.recommendation }}</div>
-
-                <div class="text-capitalize">Level of evidence:</div>
-                <div>{{ interaction.evidenceLevel }}</div>
-
-                <div class="text-capitalize" v-if="!isPrimaryMaterial && interaction.note">Note:</div>
-                <div v-if="!isPrimaryMaterial && interaction.note">{{ interaction.note }}</div>
-
-                <div class="text-capitalize">Summary:</div>
-                <div v-html="txtWithRefs(interaction.summary)" ref="summary"></div>
-
-                <div class="text-capitalize">What to monitor:</div>
-                <div> 
-                    <span class="text-capitalize">Lab tests: </span>
-                    <span>{{ interaction.monitor.labTests }}</span>
-
-                    <span class="text-capitalize" v-if="interaction.monitor.otherTests">Other tests: </span>
-                    <span v-if="interaction.monitor.otherTests">{{ interaction.monitor.otherTests }}</span>
-
-                    <span class="text-capitalize" v-if="interaction.monitor.symptons">Symptons: </span>
-                    <span v-if="interaction.monitor.symptons">{{ interaction.monitor.symptons }}</span>
-                </div>
-
-                <div class="text-capitalize">Review of studies:</div>
-                <div
-                    v-html="txtWithRefs(interaction.reviewOfStudies)"
-                    ref="reviewOfStudies"
-                ></div>
-
-                <div class="text-capitalize" v-if="material.pathways.length">Pathways:</div>
-                <div v-if="material.pathways.length">
-                    <p ref="pathway">
-                        <span class="font-weight-medium">
+            <main v-if="!isLoading && interaction && material">
+                <v-card class="v-interaction-details-content px-2">
+                    <v-card-title class="v-interaction-details-title px-0 justify-center my-4">
+                        <h2>
+                            {{ interaction.side1Material.name }}
+                            <span>&amp;</span>
                             {{ material.name }}
-                        </span>
-                        is metabolized by the enzymes:
-                        <span v-for="(pathway, idx) in material.pathways" :key="idx">
-                            <span>{{ idx === 0 ? '' : ',' }} </span>
-                            <span class="text-uppercase">{{ pathway.name }} </span>
-                            <sub>{{ getMaterialRefNums(pathway.references) }}</sub>
-                        </span>
-                    </p>
-                    <div ref="pathway2">
-                        <p v-if="relevantSide1Pathways.length">
-                            <span class="font-weight-medium">{{ interaction.side1Material.name }}</span>
-                            effect on the enzymes:
-                            <span v-for="(pathway, idx) in relevantSide1Pathways" :key="idx">
-                                <span>{{ idx === 0 ? '' : ',' }} </span>
-                                <span class="text-uppercase">{{ pathway.name }}</span>
-                            </span>
-                        </p>
-                        <div v-for="(pathway, idx) in relevantSide1Pathways" :key="'pathway' + idx">
-                            <h6>{{ pathway.name }}</h6>
-                            <p v-highlight-text:[material.name] v-html="txtWithRefs(pathway.influence, true)" />
+                        </h2>
+                    </v-card-title>
+
+                    <div class="text-capitalize">Recommendation:</div>
+                    <div v-recommendation-color>{{ interaction.recommendation }}</div>
+
+                    <div class="text-capitalize">Level of evidence:</div>
+                    <div>{{ interaction.evidenceLevel }}</div>
+
+                    <div class="text-capitalize" v-if="!isPrimaryMaterial && interaction.note">Note:</div>
+                    <div v-if="!isPrimaryMaterial && interaction.note">{{ interaction.note }}</div>
+
+                    <div class="text-capitalize">Summary:</div>
+                    <div v-html="txtWithRefs(interaction.summary)" ref="summary"></div>
+
+                    <div
+                        class="text-capitalize"
+                        v-if="interaction.monitor.labTests || interaction.monitor.otherTests || interaction.monitor.symptons"
+                    >
+                        What to monitor:
+                    </div>
+                    <div> 
+                        <div v-if="interaction.monitor.labTests" class="mb-2">
+                            <span class="text-capitalize">Lab tests: </span>
+                            <span>{{ interaction.monitor.labTests }}</span>
                         </div>
-                        <div>
-                            <p>
-                                There is no evidence regarding the effect of {{ interaction.side1Material.name }} on
-                                <span v-for="(pathway, idx) in unRelevantSide1Pathways" :key="'unrelevantPathway' + idx">{{ (idx === 0) ? '' : ', ' }}{{ pathway.name.toUpperCase() }}</span> activity.
-                            </p>
+
+                        <div v-if="interaction.monitor.otherTests" class="mb-2">
+                            <span class="text-capitalize">Other tests: </span>
+                            <span>{{ interaction.monitor.otherTests }}</span>
+                        </div>
+
+                        <div v-if="interaction.monitor.symptons" class="mb-2">
+                            <span class="text-capitalize">Symptons: </span>
+                            <span>{{ interaction.monitor.symptons }}</span>
                         </div>
                     </div>
-                </div>
-               
-                <reference-table
-                    class="refs-table"
-                    :isInteraction="true"
-                    :references="combinedRefs"
-                    v-if="combinedRefs.length"
-                />
 
-                <div class="text-capitalize">Side 1 material:</div>
-                <div>
-                    <router-link
-                        :to="`/material/${interaction.side1Material._id}`"
-                    >
-                        {{ interaction.side1Material.name }}
-                    </router-link>
-                </div>
+                    <div class="text-capitalize">Review of studies:</div>
+                    <div
+                        v-html="txtWithRefs(interaction.reviewOfStudies)"
+                        ref="reviewOfStudies"
+                    ></div>
 
-                <div class="text-capitalize">Side 2 material:</div>
-                <div>
-                    <router-link :to="`/material/${material._id}`">
-                        {{ material.name }}
-                    </router-link>
-                </div>
+                    <div class="text-capitalize" v-if="material.pathways.length">Pathways:</div>
+                    <div v-if="material.pathways.length">
+                        <p ref="pathway">
+                            <span class="font-weight-medium">
+                                {{ material.name }}
+                            </span>
+                            is metabolized by the enzymes:
+                            <span v-for="(pathway, idx) in material.pathways" :key="idx">
+                                <span>{{ idx === 0 ? '' : ',' }} </span>
+                                <span class="text-uppercase">{{ pathway.name }} </span>
+                                <sub>{{ getMaterialRefNums(pathway.references) }}</sub>
+                            </span>
+                        </p>
+                        <div ref="pathway2">
+                            <p v-if="relevantSide1Pathways.length">
+                                <span class="font-weight-medium">{{ interaction.side1Material.name }}</span>
+                                effect on the enzymes:
+                                <span v-for="(pathway, idx) in relevantSide1Pathways" :key="idx">
+                                    <span>{{ idx === 0 ? '' : ',' }} </span>
+                                    <span class="text-uppercase">{{ pathway.name }}</span>
+                                </span>
+                            </p>
+                            <div v-for="(pathway, idx) in relevantSide1Pathways" :key="'pathway' + idx">
+                                <h6>{{ pathway.name }}</h6>
+                                <p v-highlight-text:[material.name] v-html="txtWithRefs(pathway.influence, true)" />
+                            </div>
+                            <div>
+                                <p>
+                                    There is no evidence regarding the effect of {{ interaction.side1Material.name }} on
+                                    <span v-for="(pathway, idx) in unRelevantSide1Pathways" :key="'unrelevantPathway' + idx">{{ (idx === 0) ? '' : ', ' }}{{ pathway.name.toUpperCase() }}</span> activity.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <reference-table
+                        class="refs-table"
+                        :isInteraction="true"
+                        :references="combinedRefs"
+                        v-if="combinedRefs.length"
+                    />
 
-            </v-card>
-            <icons-map />
+                    <div class="text-capitalize">Side 1 material:</div>
+                    <div>
+                        <router-link
+                            :to="`/material/${interaction.side1Material._id}`"
+                        >
+                            {{ interaction.side1Material.name }}
+                        </router-link>
+                    </div>
+
+                    <div class="text-capitalize">Side 2 material:</div>
+                    <div>
+                        <router-link :to="`/material/${material._id}`">
+                            {{ material.name }}
+                        </router-link>
+                    </div>
+
+                </v-card>
+                <icons-map />
+            </main>
+            <entity-not-found 
+                entity="interaction or side 2 material"
+                v-if="!isLoading && (!interaction || !material)"
+            />
+            <loader class="v-interaction-details-loader" v-if="isLoading" />
         </div>
     </section>
 </template>
@@ -116,6 +131,8 @@
 import { interactionService } from '@/services/interaction.service';
 import referenceTable from '@/cmps/common/ReferenceTable';
 import iconsMap from '@/cmps/general/IconsMap';
+import entityNotFound from '@/cmps/general/EntityNotFound';
+import loader from '@/cmps/general/LoadingCmp';
 
 export default {
     side1Refs: [],
@@ -125,7 +142,8 @@ export default {
             material: null,
             interactionRefs: [],
             side2Refs: [],
-            side1Pathways: []
+            side1Pathways: [],
+            isLoading: false
         };
     },
     computed: {
@@ -159,21 +177,19 @@ export default {
     },
     methods: {
         async getVinteractionSides() {
+            this.isLoading = true;
             const { id, matId } = this.$route.params;
-            const [interaction, material] = await Promise.all([
+            const [interaction, side2Material] = await Promise.all([
                 this.$store.dispatch({ type: 'loadInteraction', intId: id }),
                 this.$store.dispatch({ type: 'loadMaterial', matId }),
             ]);
             this.interaction = interaction;
-            this.material = material;
+            this.material = side2Material;
+            this.isLoading = false;
             if (this.material) {
-                for (let i = 0; i < material.refs.length; i++) {
-                    const ref = material.refs[i];
-                    const idx = this.side2Refs.findIndex(matRef => matRef.pubmedId === ref.pubmedId);
-                    if (idx === -1) {
-                        this.side2Refs.push(ref);
-                    }
-                }
+                this.side2Refs = this.material.pathwayRefs.filter((ref, idx, refs) => {
+                    return refs.findIndex(currRef => currRef.pubmedId === ref.pubmedId) === idx;
+                });
             }
             if (this.interaction) {
                 await this.getReferences();
@@ -191,19 +207,19 @@ export default {
                 const idx = this.combinedRefs.findIndex(ref => pubmedIds[i] === ref.pubmedId);
                 refsStr += (idx + 1) + ', ';
             }
-            return `(${refsStr.split(', ').filter(ref => ref).sort((a, b) => a - b).join(', ')})`;
+            return `(${refsStr.split(', ').filter(str => str).sort((a, b) => a - b).join(', ')})`;
         },
         async getReferences() {
             const matId = this.interaction.side1Material._id;
-            const material = await this.$store.dispatch({
+            const side1Material = await this.$store.dispatch({
                 type: 'loadMaterial',
                 matId,
             });
-            this.$options.side1Refs = material.refs;
-            this.interactionRefs = material.refs.filter((ref) =>
+            this.$options.side1Refs = side1Material.refs;
+            this.interactionRefs = side1Material.refs.filter((ref) =>
                 this.interaction.refs.includes(ref.draftIdx)
             );
-            this.side1Pathways = material.pathways;
+            this.side1Pathways = side1Material.pathways;
             this.sortRefs();
         },
         sortRefs() {
@@ -314,6 +330,8 @@ export default {
     components: {
         referenceTable,
         iconsMap,
+        loader,
+        entityNotFound
     },
 };
 </script>
