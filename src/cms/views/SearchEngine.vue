7acb33b52b2bd0d7f4f8f06adda5103035d982f3
drznr <dresnerdaniel@gmail.com>
2021-03-08 17:26:49 +0200

    same pointer for dup interaction fixed

--- src/cms/views/SearchEngine.vue
+++ src/cms/views/SearchEngine.vue
@@ -195,6 +195,7 @@ export default {
             const queryApearanceMap = {};
             if (this.materials.length === 1) return formatedInteractions;
             return formatedInteractions.reduce((acc, interaction) => {
+                interaction = JSON.parse(JSON.stringify(interaction));
                 let side1Name = '';
                 let side2Name = '';
                 if (interaction.name) {
@@ -251,6 +252,7 @@ export default {
     },
     methods: {
         groupDoubleInteractions(acc, interaction) {
+            interaction = JSON.parse(JSON.stringify(interaction));
             /// index of vinteraction between the same 2 materials
             const idx = acc.findIndex(
                 vin => (vin.side2Material && vin.side2Material._id === interaction.side2Material._id) &&
