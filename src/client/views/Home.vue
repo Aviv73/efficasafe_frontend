6c15632de4424edc5fa8c52b204589b20e70a009
drznr <dresnerdaniel@gmail.com>
2021-03-09 18:54:24 +0200

    home page re-designed

--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -4,60 +4,56 @@
             <h1>
                 <a href="/">
                     <img
-                        src="@/client/assets/imgs/logo-img.png"
-                        alt="Logo"
+                        src="@/client/assets/imgs/logo.png"
+                        alt="Efficasafe Logo"
                     />
                 </a>
+                <span>
+                    Optimizing <span class="keyword1">efficacy</span> and
+                    <span class="keyword2">safety</span>
+                    <span class="sub"> of integrated medical treatments </span>
+                </span>
             </h1>
-            <section class="home-content">
-              <div class="main-container">
-                <div class="home-content-container">
-                  <font-awesome-icon icon="chevron-left" class="chevron fa-2x" />
-                  <form @submit.prevent="" class="home-content-form">
+            <div class="home-container">
+                <form @submit.prevent="" class="home-form">
                     <input type="text" placeholder="Search herb / drug" />
                     <input type="text" placeholder="Add another" />
-                  </form>
-                  <font-awesome-icon icon="chevron-right" class="chevron fa-2x" />
-                </div>
-              </div>
-            </section>
-            <h2 class="home-subheader">
-                Optimizing efficacy and safety of integrated medical treatments
-            </h2>
+                </form>
+            </div>
         </header>
         <section class="home-stats">
-            <ul class="flex-space-between">
-                <li>
-                    <span>1150</span>
-                    Clinical trials
-                </li>
-                <li>
-                    <span>48000</span>
-                    Pre-clinical trials
-                </li>
-                <li>
-                    <span>93414</span>
-                    Articles
-                </li>
-            </ul>
+            <div class="home-container">
+                <ul class="flex-space-between">
+                    <li>
+                        <span>1,150</span>
+                        Clinical trials
+                    </li>
+                    <li>
+                        <span>48,000</span>
+                        Pre-clinical trials
+                    </li>
+                    <li>
+                        <span>93,414</span>
+                        Articles
+                    </li>
+                </ul>
+            </div>
+            <div class="home-container">
+                <p class="home-stats-quote">
+                    Finally! One site with everything in it. I believe
+                    that professionals have been waiting for something
+                    like this to come along for a very long time.
+                    <span>Dr. Jonathan Rosenzweig, MD</span>
+                </p>
+            </div>
         </section>
-        <footer class="home-footer">
-            <span class="home-footer-txt">
-                Cross-check multiple drugs, herbs and supplements in order to
-                optimize safety and efficiency of integrative medical care.
-            </span>
-            <img
-                src="@/client/assets/imgs/logo-symbol.png"
-                alt="Logo"
-                class="home-footer-logo"
-            />
-        </footer>
-        <span class="hr" />
+        <section class="home-content"></section>
     </section>
 </template>
 
 <script>
+
 export default {
-    name: 'Home',
+    name: 'Home'
 };
 </script>
