dfbf711ab6411f53a354f65b22fac95554590f21
drznr <dresnerdaniel@gmail.com>
2021-03-11 16:39:05 +0200

    main footer added

--- src/client/App.vue
+++ src/client/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <navbar />
     <router-view />
+    <main-footer />
   </div>
 </template>
 
 <script>
 import Navbar from '@/client/cmps/Navbar';
+import MainFooter from '@/client/cmps/MainFooter';
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    MainFooter
   }
 }
 </script>
---
+++ src/client/cmps/MainFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="main-footer">
+        <img src="@/client/assets/imgs/logo-symbol.png" alt="Efficasafe logo" class="main-footer-logo" />
+        <small class="main-footer-details">
+            Copyright © efficasafe | All RIghts Reserved | Site Last Updated Jun
+            2020 | Privacy Policy | Complaints Procedure Terms and Conditions |
+            Tel: +972.50.793.1881 | Fax +972.03.712.1815 | Email:
+            info@efficasafe.com
+        </small>
+    </footer>
+</template>
