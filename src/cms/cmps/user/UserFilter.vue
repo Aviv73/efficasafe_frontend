717709e05ccc7b0d8c940b9cec7dae42cb7d2203
shaharp2 <shaharp2@gmail.com>
2021-04-25 14:11:08 +0300

    try

--- src/cms/cmps/user/UserFilter.vue
+++ src/cms/cmps/user/UserFilter.vue
@@ -1,46 +1,52 @@
 <template>
-  <section class="user-filter">
-    <div class="filter-input">
-      <v-text-field label="Search User By name or email" v-model="filterBy.q" />
-    </div>
-  </section>
+    <section class="user-filter">
+        <div class="filter-input">
+            <v-text-field
+                label="Search User By name or email"
+                v-model="filterBy.q"
+            />
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      filterBy: {
-        q: "",
-      },
-    };
-  },
-  watch: {
-    filterBy: {
-      handler() {
-        this.fetchEntriesDebounced();
-      },
-      deep: true,
+    data() {
+        return {
+            filterBy: {
+                q: '',
+            },
+        };
     },
-  },
-  methods: {
-    fetchEntriesDebounced() {
-      let timerId = null;
-      clearTimeout(timerId);
-      timerId = setTimeout(() => {
-        this.emitFilterBy();
-      }, 500);
+    watch: {
+        filterBy: {
+            handler() {
+                this.fetchEntriesDebounced();
+            },
+            deep: true,
+        },
     },
-    emitFilterBy() {
-      this.$emit("emit-filter", JSON.parse(JSON.stringify(this.filterBy)));
+    methods: {
+        fetchEntriesDebounced() {
+            let timerId = null;
+            clearTimeout(timerId);
+            timerId = setTimeout(() => {
+                this.emitFilterBy();
+            }, 500);
+        },
+        emitFilterBy() {
+            this.$emit(
+                'emit-filter',
+                JSON.parse(JSON.stringify(this.filterBy))
+            );
+        },
     },
-  },
 };
 </script>
 
 <style>
 .filter-input {
-  width: 40%;
-  margin: 0 auto;
+    width: 40%;
+    margin: 0 auto;
 }
 </style>
