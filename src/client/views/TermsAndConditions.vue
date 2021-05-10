ae8a39932978522de360f51721e3325a6f439220
drznr <dresnerdaniel@gmail.com>
2021-05-10 12:52:14 +0300

    TermsAndConditions page started

--- src/client/views/TermsAndConditions.vue
+++ src/client/views/TermsAndConditions.vue
@@ -4,6 +4,233 @@
         <article>
             <div class="main-container">
                 <h1 class="page-header1">End user license agreement</h1>
+                <p>
+                    Welcome to Efficasafe.com, an
+                    <span class="font-bold"
+                        >Herb-Drug-Supplement Interaction Platform</span
+                    >, that enables doctors, pharmacists, and healthcare
+                    providers access to the most extensive, updated,
+                    evidence-based interaction data, helping them to optimize
+                    the efficacy and safety of integrative medical care.
+                </p>
+                <p>
+                    Please read through this End User License Agreement
+                    carefully as it contains important information that you
+                    should consider before using our platform.
+                </p>
+                <p>
+                    This End User License Agreement (“Agreement”) is a binding
+                    agreement between you (“End User” or “you”) and Naturopedia
+                    LTD or the website Efficasafe.com (“we” or “Efficasafe”).
+                    This Agreement governs your use of Efficasafe.com software
+                    (including all related user documentation provided to you,
+                    the “Application”). The Application is licensed, not sold,
+                    to you. Collectively, the database architectures, database,
+                    algorithms, mobile applications and websites comprise the
+                    system hereinafter referred to as the “Platform”.
+                </p>
+                <p>
+                    BY CLICKING THE “I AGREE”
+                    BUTTON/DOWNLOADING/INSTALLING/USING THE APPLICATION, YOU (A)
+                    ACKNOWLEDGE THAT YOU HAVE READ AND UNDERSTAND THIS
+                    AGREEMENT; (B) REPRESENT THAT YOU ARE 18 YEARS OF AGE OR
+                    OLDER/OF LEGAL AGE TO ENTER INTO A BINDING AGREEMENT; AND
+                    (C) ACCEPT THIS AGREEMENT AND AGREE THAT YOU ARE LEGALLY
+                    BOUND BY ITS TERMS. IF YOU DO NOT AGREE TO THESE TERMS, DO
+                    NOT DOWNLOAD/INSTALL/USE THE APPLICATION AND DELETE IT FROM
+                    YOUR DEVICE.
+                </p>
+                <p>
+                    This Application is only available for individuals aged 18
+                    years or older. IF YOU ARE NOT AT LEAST 18 YEARS OLD, OR THE
+                    LEGAL AGE REQUIRED TO FORM A BINDING CONTRACT IN YOUR
+                    JURISDICTION IF THAT AGE IS GREATER THAN 18, YOU MAY NOT
+                    CLICK THE “I AGREE” BUTTON UNLESS YOU HAVE OBTAINED APPROVAL
+                    OF THIS AGREEMENT FROM YOUR PARENT OR LEGAL GUARDIAN. IF YOU
+                    DO NOT AGREE OR HAVE NOT OBTAINED SUCH APPROVAL, YOU MUST
+                    SELECT THE “I DISAGREE” BUTTON AND YOU MAY NOT CONTINUE TO
+                    INSTALL, DOWNLOAD, COPY, ACCESS OR USE THE APPLICATION.
+                </p>
+                <p>
+                    This Web site - Efficasafe.com - is for general health
+                    information only and is intended for professional use only.
+                    This Web site is not to be used as a substitute for medical
+                    advice, diagnosis or treatment of any health condition or
+                    problem. Users of this Web site should not rely on
+                    information provided on this Web site for their own health
+                    problems. Any questions regarding your own health should be
+                    addressed to your own physician or other healthcare
+                    provider. Naturopedia LTD and the Efficasafe website make no
+                    warranties nor express or implied representations whatsoever
+                    regarding the accuracy, completeness, timeliness,
+                    comparative or controversial nature, or usefulness of any
+                    information contained or referenced on this Web site.
+                    Naturopedia LTD and the Efficasafe website do not assume any
+                    risk whatsoever for your use of this website or the
+                    information contained herein. Health-related information
+                    changes frequently and therefore information contained on
+                    this Web site may be outdated, incomplete or incorrect.
+                    Statements made about products have not been evaluated by
+                    the Food and Drug Administration. Use of this Web site does
+                    not create an expressed or implied physician-patient
+                    relationship. You are hereby advised to consult with a
+                    physician or other professional health-care provider prior
+                    to making any decisions, or undertaking any actions or not
+                    undertaking any actions related to any health care problem
+                    or issue you might have at any time, now or in the future.
+                    In using this website you agree that neither Naturopedia LTD
+                    and the Efficasafe website nor any other party is or will be
+                    liable or otherwise responsible for any decision made or any
+                    action taken or any action not taken due to your use of any
+                    information presented at this website.
+                </p>
+                <h2>1. Fees and payment.</h2>
+                <h3>1.1 Subscriptions.</h3>
+                <p>
+                    Access to the Application is purchased via license
+                    subscriptions on either a monthly or annual basis. With
+                    respect to monthly license subscriptions for the
+                    Application, (i) such subscriptions (i.e. the number of End
+                    Users) may be decreased during the calendar month and (ii)
+                    such subscriptions (i.e. the number of End Users) may be
+                    increased during the calendar month at the then effective
+                    pricing and will end on the same date as the original
+                    license subscriptions. Customer agrees that its purchases of
+                    license subscriptions are not contingent on the delivery of
+                    any future functionality or features, or dependent on any
+                    oral or written public comments made by Efficasafe regarding
+                    future functionality or features.
+                </p>
+                <h3>1.2 Fees.</h3>
+                <p>
+                    Customer will pay all fees specified in the on-line order(s)
+                    form completed and submitted by Customer (the “Order
+                    Forms”). Except as otherwise specified herein or in an Order
+                    Form, (i) fees are based on license subscriptions purchased
+                    and not actual usage, (ii) payment obligations are
+                    non-cancelable (except as otherwise provided immediately
+                    below) and fees paid are non-refundable, and (iii) monthly
+                    license subscriptions (but not annual license subscriptions)
+                    may be decreased with the corresponding decreased fees
+                    applicable in the immediately following month. Customer
+                    acknowledges and agrees that Efficasafe may revise its fee
+                    schedule at any time and from time to time upon 30-days
+                    prior written notice (posted on the website); provided, that
+                    such changes shall not go into effect for annual
+                    subscriptions until renewal thereof.
+                </p>
+                <h3>1.3 Monthly Subscriptions: Invoicing and Payment.</h3>
+                <p>
+                    With respect to monthly license subscriptions, Customer will
+                    provide Efficasafe with valid and updated credit card
+                    information. Customer authorizes Efficasafe to charge such
+                    credit card for all license subscriptions listed in the
+                    Order Form(s) for the initial subscription term and any
+                    renewal subscription term(s) in advance on the first of each
+                    month (prorated for the initial month of any license
+                    subscriptions). Customer is responsible for providing
+                    complete and accurate billing and contact information to
+                    Efficasafe and notifying Efficasafe of any changes to such
+                    information.
+                </p>
+                <h3>1.4 Annual Subscriptions: Invoicing and Payment.</h3>
+                <p>
+                    With respect to annual license subscriptions, Customer will
+                    provide Efficasafe with valid and updated credit card
+                    information, or with a valid purchase order or alternative
+                    document reasonably acceptable to Efficasafe. If Customer
+                    provides credit card information to Efficasafe, Customer
+                    authorizes Efficasafe to charge such credit card for all
+                    license subscriptions listed in the Order Form(s) for the
+                    initial subscription term and any renewal subscription
+                    term(s). Such charges shall be made annually in advance. If
+                    the Order Form specifies that payment will be by a method
+                    other than a credit card, Efficasafe will invoice Customer
+                    in advance and otherwise in accordance with the relevant
+                    Order Form. Unless otherwise stated in the Order Form,
+                    invoiced fees are due 30 days from the invoice date.
+                    Customer is responsible for providing complete and accurate
+                    billing and contact information to Efficasafe and notifying
+                    Efficasafe of any changes to such information.
+                </p>
+                <h3>1.5 Suspension of Service and Acceleration.</h3>
+                <p>
+                    If any charge owing by Customer under this or any other
+                    agreement for services is 30 days or more overdue, (or 10 or
+                    more days overdue in the case of amounts Customer has
+                    authorized Efficasafe to charge to Customer’s credit card),
+                    Efficasafe may, without limiting its other rights and
+                    remedies, accelerate Customer’s unpaid fee obligations under
+                    such agreements so that all such obligations become
+                    immediately due and payable, and suspend Services until such
+                    amounts are paid in full, provided that, other than for
+                    customers paying by credit card or direct debit whose
+                    payment has been declined, Efficasafe will give Customer at
+                    least 10 days’ prior notice that its account is overdue,
+                    before suspending services to Customer.
+                </p>
+                <h3>1.6 Payment Disputes.</h3>
+                <p>
+                    Efficasafe will not exercise its rights under the “Overdue
+                    Charges” or “Suspension of Service and Acceleration” section
+                    above if Customer is disputing the applicable charges
+                    reasonably and in good faith and is cooperating diligently
+                    to resolve the dispute.
+                </p>
+                <h3>1.7 Taxes.</h3>
+                <p>
+                    Efficasafe's fees do not include any taxes, levies, duties
+                    or similar governmental assessments of any nature,
+                    including, for example, value-added, sales, use or
+                    withholding taxes, assessable by any jurisdiction whatsoever
+                    (collectively, “Taxes”). Customer is responsible for paying
+                    all Taxes associated with its purchases hereunder. If
+                    Efficasafe has the legal obligation to pay or collect Taxes
+                    for which Customer is responsible under this section,
+                    Efficasafe will invoice Customer and Customer will pay that
+                    amount unless Customer provides Efficasafe with a valid tax
+                    exemption certificate authorized by the appropriate taxing
+                    authority. For clarity, Efficasafe is solely responsible for
+                    taxes assessable against it based on its income, property
+                    and employees.
+                </p>
+                <h2>2. 14-Day Free Trial.</h2>
+                <p>
+                    Customer will receive a temporary license for full access to
+                    the Efficasafe website for 14 days (the “Trial Period”)
+                    subject to all of the terms and conditions set forth in this
+                    Master License Agreement (other than the terms and
+                    conditions of Section 3 above) and the End User Agreements.
+                    After 10 days, Customer will receive notice that the Trial
+                    Period will end in 4 days and Customer will be offered the
+                    opportunity to purchase license subscriptions pursuant to
+                    Order Form(s) at any time prior to the expiration of the
+                    Trial Period. If Customer chooses not to purchase license
+                    subscription(s), access to the Application will terminate
+                    the expiration of the Trial Period. “End User” means, in the
+                    case of an individual accepting this Master Service
+                    Agreement on their own behalf, such individual, or, in the
+                    case of an individual accepting this Master License
+                    Agreement on behalf of a legal entity (e.g. a corporation or
+                    limited liability company), an individual (i) who is
+                    authorized by Customer to use the Application, (ii) who has
+                    agreed to an End User Agreement and (iii), for whom Customer
+                    has purchased a subscription (except during the Trial Period
+                    when no purchase is necessary). End Users may include, for
+                    example, employees, consultants, contractors and agents of
+                    Customer, and third parties with which Customer transacts
+                    business.
+                </p>
+                <h2>3. License Grant.</h2>
+                <p>
+                    Subject to the terms of this Agreement, Naturopedia LTD and
+                    Efficasafe.com grant you a limited, non-exclusive and
+                    nontransferable license to use the Application for your
+                    personal use on a device owned or otherwise controlled by
+                    you (“Device”) strictly in accordance with this Agreement
+                    and any other associated documentation provided to you.
+                </p>
+                <h2>4. License Restrictions.</h2>
             </div>
         </article>
     </section>
@@ -14,7 +241,7 @@ import PageHeader from '@/client/cmps/shared/PageHeader';
 
 export default {
     components: {
-        PageHeader
-    }    
-}
+        PageHeader,
+    },
+};
 </script>
