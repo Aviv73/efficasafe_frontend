2d0409a8f4e26c52d3a2af6ade40b43918378d9e
drznr <dresnerdaniel@gmail.com>
2021-05-12 16:13:58 +0300

    some minor design changes

--- src/client/cmps/MainFooter.vue
+++ src/client/cmps/MainFooter.vue
@@ -2,10 +2,12 @@
     <footer class="main-footer">
         <img src="@/client/assets/imgs/logo-symbol.png" alt="Efficasafe logo" class="main-footer-logo" />
         <small class="main-footer-details">
-            Copyright © efficasafe | All RIghts Reserved | Site Last Updated {{ lastSiteUpdate | moment('LL') }}
-            | <router-link to="/privacy-statement" class="link">Privacy Policy</router-link> | Complaints Procedure
+            <span class="d-block">
+                Copyright © efficasafe | All RIghts Reserved | Site Last Updated {{ lastSiteUpdate | moment('LL') }}
+            </span>
             <router-link to="/terms-and-conditions" class="link">Terms and Conditions</router-link>
-            | Email: <a href="mailto:info@efficasafe.com" class="link">info@efficasafe.com</a>
+            | <router-link to="/privacy-statement" class="link">Privacy Policy</router-link>
+            | Email: <a href="mailto:info@efficasafe.com" class="link">support@efficasafe.com</a>
         </small>
     </footer>
 </template>
