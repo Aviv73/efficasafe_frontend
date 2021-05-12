66f017e0deeda70a98ccf5aad90daea76200ae68
drznr <dresnerdaniel@gmail.com>
2021-05-12 15:18:07 +0300

    footer details fixed

--- src/client/cmps/MainFooter.vue
+++ src/client/cmps/MainFooter.vue
@@ -2,11 +2,20 @@
     <footer class="main-footer">
         <img src="@/client/assets/imgs/logo-symbol.png" alt="Efficasafe logo" class="main-footer-logo" />
         <small class="main-footer-details">
-            Copyright © efficasafe | All RIghts Reserved | Site Last Updated Jun
-            2020 | <router-link to="/privacy-statement" class="link">Privacy Policy</router-link> | Complaints Procedure
+            Copyright © efficasafe | All RIghts Reserved | Site Last Updated {{ lastSiteUpdate | moment('LL') }}
+            | <router-link to="/privacy-statement" class="link">Privacy Policy</router-link> | Complaints Procedure
             <router-link to="/terms-and-conditions" class="link">Terms and Conditions</router-link>
-            | Tel: +972.50.793.1881 | Fax +972.03.712.1815 | Email:
-            info@efficasafe.com
+            | Email: <a href="mailto:info@efficasafe.com" class="link">info@efficasafe.com</a>
         </small>
     </footer>
 </template>
+
+<script>
+export default {
+    computed: {
+        lastSiteUpdate() {
+            return new Date(document.lastModified);
+        }
+    }
+}
+</script>
