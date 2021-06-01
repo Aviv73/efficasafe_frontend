e7e849ebcf6a12bc31daa9c0b39a54d2e73b5167
drznr <dresnerdaniel@gmail.com>
2021-06-01 12:23:50 +0300

    conflixt fixed

--- src/cms/config/index.js
+++ src/cms/config/index.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export default {
     auth0ClientId: process.env.VUE_APP_AUTH0_CLIENT_ID,
     auth0BaseURL: process.env.VUE_APP_AUTH0_BASE_URL
-=======
-module.exports = {
-    "auth0ClientId": process.env.VUE_APP_AUTH0_CLIENT_ID,
-    "auth0BaseURL": process.env.VUE_APP_AUTH0_BASE_URL
->>>>>>> staging
 }
