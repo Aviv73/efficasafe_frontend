67c9f4fc95a21b91c458b4fd93f7a0e8130a63b4
drznr <dresnerdaniel@gmail.com>
2021-06-01 10:22:07 +0300

    monitor design change

--- src/client/cmps/search-engine/MonitorSummary.vue
+++ src/client/cmps/search-engine/MonitorSummary.vue
@@ -5,39 +5,17 @@
                 <div class="interaction-preview-header table-row">
                     <span class="monitor-summary-header table-col">
                         <span class="capsule">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="cap cap-left"
-                            viewBox="0 0 24.192 48.001"
-                        >
-                            <path 
-                                fill="#b1b1b1"
-                                d="M24.192,48h-.221A23.973,23.973,0,0,1,0,24,23.96,23.96,0,0,1,23.972,0h.221V4.132a21.046,21.046,0,0,0-6.841,1.532A20.58,20.58,0,0,0,10.8,9.929a19.82,19.82,0,0,0-4.414,6.326,19.338,19.338,0,0,0,0,15.492A19.81,19.81,0,0,0,10.8,38.071a20.585,20.585,0,0,0,6.548,4.265,21.043,21.043,0,0,0,6.841,1.532V48Z"
-                                transform="translate(24.192 48.001) rotate(180)"
-                            />
-                        </svg>
                             What to monitor for all interactions
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="cap cap-right"
-                            viewBox="0 0 24.192 48.001"
-                        >
-                            <path
-                                fill="#b1b1b1"
-                                d="M24.192,0h-.221A23.973,23.973,0,0,0,0,24,23.96,23.96,0,0,0,23.971,48h.221V43.869a21.046,21.046,0,0,1-6.841-1.532A20.58,20.58,0,0,1,10.8,38.072a19.82,19.82,0,0,1-4.414-6.326,19.338,19.338,0,0,1,0-15.492A19.81,19.81,0,0,1,10.8,9.93a20.585,20.585,0,0,1,6.548-4.265,21.042,21.042,0,0,1,6.841-1.532V0Z"
-                            />
-                        </svg>
+                            <span
+                                class="de-activator"
+                            >
+                                <chevron-up-icon class="opened" />
+                                <chevron-down-icon class="closed" />
+                            </span>
                         </span>
                     </span>
                     <span class="table-col" />
-                    <span class="table-col">
-                        <span
-                            class="de-activator"
-                        >
-                            <chevron-up-icon class="opened" />
-                            <chevron-down-icon class="closed" />
-                        </span>
-                    </span>
+                    <span class="table-col" />
                 </div>
             </template>
             <template #content>
