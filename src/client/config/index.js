76cb0483440f7f6670e540f8be6fc3cddb6c535c
drznr <dresnerdaniel@gmail.com>
2021-06-17 16:34:47 +0300

    conf syntax changed

--- src/client/config/index.js
+++ src/client/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "auth0ClientId": process.env.VUE_APP_AUTH0_CLIENT_ID,
-    "auth0BaseURL": process.env.VUE_APP_AUTH0_BASE_URL
+    auth0ClientId: process.env.VUE_APP_AUTH0_CLIENT_ID,
+    auth0BaseURL: process.env.VUE_APP_AUTH0_BASE_URL
 }
