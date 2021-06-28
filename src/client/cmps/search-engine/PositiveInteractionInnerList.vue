da44795d4ba510fb207127bb682cc15bc85cedb0
drznr <dresnerdaniel@gmail.com>
2021-06-28 12:20:03 +0300

    positive boosters refactor done

--- src/client/cmps/search-engine/PositiveInteractionInnerList.vue
+++ src/client/cmps/search-engine/PositiveInteractionInnerList.vue
@@ -75,6 +75,34 @@ export default {
                     interaction.name = `${interaction.side1Material.name} & ${interaction.side2Material.name}`;
                 }
             });
+            const seenVinteractionsMap = {};
+            interactions.forEach(interaction => {
+                const side1Queries = this.$store.getters.materialNamesMap[interaction.side1Material.name];
+                    const side1Name = side1Queries ? side1Queries.join(', ') : interaction.side1Material.name;
+                    if (interaction.side2Material) {
+                        const userQuery = this.$store.getters.materialNamesMap[interaction.side2Material.name];
+                        const interactionName = `${side1Name} & ${userQuery ? userQuery.join(', ') : interaction.side2Material.name}`;
+                        
+                        if (!seenVinteractionsMap[interactionName]) seenVinteractionsMap[interactionName] = [ interaction ];
+                        else seenVinteractionsMap[interactionName].push(interaction);
+                    } else {
+                        const matchingMaterial = this.materials.find(m => m.labels.some(l => l._id === interaction.side2Label._id));
+                        const userQueries = this.$store.getters.materialNamesMap[matchingMaterial.name];
+                        const side2Name = userQueries ? userQueries.join(', ') : matchingMaterial.name;
+                        const interactionName = `${side1Name} & ${side2Name}`;
+                        
+                        if (!seenVinteractionsMap[interactionName]) seenVinteractionsMap[interactionName] = [ interaction ];
+                        else seenVinteractionsMap[interactionName].push(interaction);
+                    }
+            });
+            Object.values(seenVinteractionsMap).forEach(seenInteractions => {
+                if (seenInteractions.length <= 1) return;
+                const leadInteraction = this.getMoreSeverInteraction(seenInteractions);
+                seenInteractions = seenInteractions.filter(i => i._id !== leadInteraction._id);
+                seenInteractions.forEach(({ _id }) => {
+                    interactions = interactions.filter(i => i._id !== _id);
+                });
+            });
             interactions.sort((a, b) => {
                 return (map[b.recommendation] - map[a.recommendation]) * -1 ||
                 (a.evidenceLevel.toLowerCase().localeCompare(b.evidenceLevel.toLowerCase())) ||
@@ -87,6 +115,13 @@ export default {
             }
 
             return interactions;
+        },
+        getMoreSeverInteraction(interactions) {
+            const map = interactionUIService.getRecommendationOrderMap();
+            interactions.slice().sort((a, b) => {
+                return (map[a.recommendation.toLowerCase()] > map[b.recommendation.toLowerCase()]) ? -1 : (map[a.recommendation.toLowerCase()] < map[b.recommendation.toLowerCase()]) ? 1 : 0;
+            });
+            return interactions[0];
         }
     },
     created() {
