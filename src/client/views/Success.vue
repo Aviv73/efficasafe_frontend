696d3393918f0548dc2fc43dd0f9e1235eaf6f36
StavPartouche <stave1995@gmail.com>
2022-01-10 14:16:10 +0200

    new payment test with HK 4

--- src/client/views/Success.vue
+++ src/client/views/Success.vue
@@ -1,9 +1,29 @@
 <template>
-  <section class="success-page">
-    <img class="logo" src="@/client/assets/imgs/flat-logo.png" alt="Logo" />
-    <h2>Congratulations!</h2>
-    <h3>We’re glad that you’ve decided to join us</h3>
-    <button class="btn" @click="$router.push('/search')">Let’s search Interactions!</button>
-  </section>
+    <section class="success-page">
+        <img class="logo" src="@/client/assets/imgs/flat-logo.png" alt="Logo" />
+        <h2>Congratulations!</h2>
+        <h3>We’re glad that you’ve decided to join us</h3>
+        <button class="btn" @click="$router.push('/search')">
+            Let’s search Interactions!
+        </button>
+    </section>
 </template>
 
+<script>
+import { userService } from '@/cms/services/user.service';
+
+export default {
+    computed: {
+        loggedInUser() {
+            return this.$store.getters.loggedInUser;
+        },
+    },
+    async created() {
+        await this.$store.dispatch('getUserInfo');
+        const user = await userService.getById(this.loggedInUser._id);
+        this.$store.commit({ type: 'setLoggedInUser', user });
+        await userService.updateSession(user)
+        console.log('DONE!!!!');
+    },
+};
+</script>
