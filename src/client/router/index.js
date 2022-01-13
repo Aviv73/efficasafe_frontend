15283a078835c0d4744e6805072159d98fa583a3
StavPartouche <stave1995@gmail.com>
2022-01-13 13:26:06 +0200

    GTM test

--- src/client/router/index.js
+++ src/client/router/index.js
@@ -242,4 +242,8 @@ router.beforeEach(async (to, from, next) => {
   next();
 });
 
+router.afterEach((to) => {
+  window.dataLayer.push({'event': 'customPage', virtualPagePath: to.fullPath})
+})
+
 export default router;
