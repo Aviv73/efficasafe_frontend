fe9f51c5e9f0c4c7ae4c0c9e84f39aa5364b9c09
StavPartouche <stave1995@gmail.com>
2022-08-24 16:47:33 +0300

    video size and admin only user edit

--- src/client/cmps/UpdateMsg.vue
+++ src/client/cmps/UpdateMsg.vue
@@ -2,11 +2,9 @@
     <div class="update-msg-container no-print" :class="{'show-update-msg': showMsg}">
         <close-icon class="close-btn" @click="onCloseMsg"/>
         <h2>New content is available!</h2>
-        <!-- <p>Press <span class="highlight">Ctrl+F5</span> or <span class="highlight">Command+Shift+R</span> on mac to update.</p> -->
-        <p>
-            Please reload the page
-        </p>
-        <button @click="reloadPage" class="reload-btn">Reload</button>
+        <p>Press <span class="highlight">Ctrl+F5</span> or <span class="highlight">Command+Shift+R</span> on mac to update.</p>
+        <!-- <p>Please reload the page</p>
+        <button @click="reloadPage" class="reload-btn">Reload</button> -->
         <p class="note">*Not updating will lead to a poor user experience.</p>
     </div>
 </template>
