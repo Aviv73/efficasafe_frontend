32a98faa00e5f8c89d9b84fb5aba6c7b0d72aabb
Aviv Issachar <avivi@misterbit.co.il>
2023-06-20 17:32:49 +0300

    ..

--- src/client/cmps/search-engine/InteractionPreview.vue
+++ src/client/cmps/search-engine/InteractionPreview.vue
@@ -15,7 +15,7 @@
           >
             <span class="table-col" :class="{ 'flex-align-center': isDuplicate, 'off-interaction-col-1': !isAllowed }">
               <interaction-capsules
-                :hoverMsg="hoverMsg || (interaction.isCompoundGroup === false) && interaction.name || ''"
+                :hoverMsg="hoverMsg || (interaction.isCompoundGroup === false) && (interaction.originLabel && interaction.originLabel.name) || ''"
                 :name="interactionName || ''"
                 :color="getInteractionColor(interaction)"
                 :vInteractionCount="newLength || getVinteractionsCount(interaction)"
@@ -160,7 +160,7 @@
         >
           <p class="msg" v-if="interaction.isCompoundGroup === false">There are different interactions, dependent on {{ getSide2Name(interaction.name) }} use:</p>
           <div v-for="(vInteraction, index) in interaction.vInteractions" :key="index">
-            <interaction-preview :hoverMsg="(interaction.isCompoundGroup === false) && interaction.name || ''" :interaction="vInteraction" :materials="materials" :isCompoundPart="isCompoundPart || interaction.isCompoundGroup" :isDuplicate="interaction.isCompoundGroup === false" :link="link" :idx="index" :parent-idx="idx" is-child />
+            <interaction-preview :hoverMsg="(interaction.isCompoundGroup === false) && (interaction.originLabel && interaction.originLabel.name) || ''" :interaction="vInteraction" :materials="materials" :isCompoundPart="isCompoundPart || interaction.isCompoundGroup" :isDuplicate="interaction.isCompoundGroup === false" :link="link" :idx="index" :parent-idx="idx" is-child />
           </div>
         </div>
       </template>
@@ -589,6 +589,7 @@ export default {
     }
   },
   async created() {
+    if (this.interaction.isCompoundGroup === false) console.log(this.interaction)
     // this.getPathwayRefsCount();
     this.primarySideInView = this.$store.getters.firstInteractionSide;
     eventBus.$on(EV_sortby_side_swaped, this.swapSideNames);
