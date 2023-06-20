e09f6d7e502d127b7117dc2610a98eae6d8e9ab4
Aviv Issachar <avivi@misterbit.co.il>
2023-06-20 17:10:55 +0300

    ..

--- src/client/views/PaymentFailed.vue
+++ src/client/views/PaymentFailed.vue
@@ -3,7 +3,7 @@
     <img class="logo" src="@/client/assets/imgs/flat-logo.png" alt="Logo" />
     <h2>Somthing went wrong</h2>
     <h3>For some reason the payment did not go through, please try again</h3>
-    <div class="failed-btns">
+    <div class="failed-btns go-search-btn">
       <button class="btn" @click="$router.push('/subscribe')">Try again</button>
       <span>or</span>
       <button class="btn" @click="$router.push('/contact')">Contact us</button>
