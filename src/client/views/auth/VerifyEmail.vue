499e5563dc00fd02c0e8cd7d2fdce1cc04aec848
Aviv Issachar <avivi@misterbit.co.il>
2023-06-21 17:09:01 +0300

    ..

--- src/client/views/auth/VerifyEmail.vue
+++ src/client/views/auth/VerifyEmail.vue
@@ -1,21 +1,33 @@
 <template>
-    <p>Verifing email...</p>
+    <p v-if="!showError">Verifing email...</p>
+    <p v-else>Something went wrong, couldn't verify email</p>
 </template>
 
 <script>
 import { userService } from '@/cms/services/user.service';
 
 export default {
+    name: 'VerifyEmail',
+    data() {
+        return {
+            showError: false
+        }
+    },
     methods: {
         async VerifyEmail() {
-            const { token } = this.$route.params;
-            const res = await userService.verifyEmail(token);
-            if (res.success){
-                const user = await this.$store.dispatch('getUserInfo');
-                await this.$store.dispatch({type: 'updateAutoPilotContact', user});
-                this.$router.push('/?congratulations=yes');
-            }else{
-                console.log('Error');
+            try {
+                const { token } = this.$route.params;
+                const res = await userService.verifyEmail(token);
+                if (res.success){
+                    const user = await this.$store.dispatch('getUserInfo');
+                    await this.$store.dispatch({type: 'updateAutoPilotContact', user});
+                    this.$router.push('/?congratulations=yes');
+                }else{
+                    this.showError = true;
+                    console.log('Error');
+                }
+            } catch (e) {
+                this.showError = true;
             }
         },
     },
